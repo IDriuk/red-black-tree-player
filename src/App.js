02b9bcb25f7366e2ab7bf3f1400617abeacda3fe
Ivan Driuk <veytarn@gmail.com>
2020-02-01 11:43:32 +0200

    init repo with create react app

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Red Black Tree Player</h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Gihub
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
