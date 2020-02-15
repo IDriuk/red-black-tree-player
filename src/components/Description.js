2479fa322a6c8221c60701883d0560a32a9bc655
Ivan Driuk <veytarn@gmail.com>
2020-02-15 19:10:43 +0200

    update requirements

--- src/components/Description.js
+++ src/components/Description.js
@@ -4,11 +4,7 @@ function Description() {
   return (
     <header className="App-header">
       <h1>Red Black Tree Player</h1>
-      <p>render tree with d3, after each change</p>
-      <p>render each change step without animation</p>
-      <p>add change animation </p>
-      <p>add play changes back without animation</p>
-      <p>play forward and back, each step with animations</p>
+      <p>Put array of numbers. Build tree in order, remove nodes randomly. Animate work of algorithm, moving and recoloring nodes, creating and updating links</p>
     </header>
   );
 }
